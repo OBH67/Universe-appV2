e8af57473c7cd5f9bcfc32129575ecd311ba7322
OBH67 <omarbalanzar113@gmail.com>
2021-08-02 01:41:03 -0700

    comment

--- src/components/infoPage/Data.js
+++ src/components/infoPage/Data.js
@@ -7,7 +7,7 @@ export const InfoObjTwo = {
     lightTextDesc: true,
     topLine: 'Universe Token',
     headline: '¿Whats new in Universe Token?',
-    description: 'The Universe Token community can participate in a vote to choose the token they want to obtain as royalties for the benefit of having our token. Allow holders to decide the token they most want.',
+    description: 'The Universe Token community will participate in a vote to choose the token they want to obtain as royalties for the benefit of having our token. Allow holders to decide the token they most want.',
     buttonLabel: 'Get started',
     imgStart: false,
     img: image,
